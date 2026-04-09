2e400df4a4917c1a7c0203b3030b4b795ecea367
James Prevett <jp@jamespre.dev>
2026-04-09 14:24:54 -0500

    Fixed config loading

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@ import { program } from 'commander';
 import $pkg from '../package.json' with { type: 'json' };
 import * as io from 'ioium/node';
 import { runGit } from './git/git.js';
+import { defaultConfigPath, loadConfig } from './git/config.js';
 
 const spam = program.name($pkg.name).version($pkg.version).description($pkg.description).option('--debug', 'Enable debug mode');
 
@@ -13,6 +14,7 @@ spam.on('option:debug', debug => {
 spam.command('pull')
 	.description('Pull changes from all managed repositories')
 	.action(async function spam_pull() {
+		loadConfig(defaultConfigPath);
 		await runGit({ concurrency: 4, args: ['pull', '--progress'] });
 	});
 
