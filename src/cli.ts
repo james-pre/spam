2e337cefa7e9d7ad9f9e7d56bc6cdd17a642af15
James Prevett <jp@jamespre.dev>
2026-04-09 14:09:58 -0500

    Added `spam pull`

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 import { program } from 'commander';
 import $pkg from '../package.json' with { type: 'json' };
 import * as io from 'ioium/node';
+import { runGit } from './git/git.js';
 
 const spam = program.name($pkg.name).version($pkg.version).description($pkg.description).option('--debug', 'Enable debug mode');
 
@@ -8,5 +9,11 @@ spam.on('option:debug', debug => {
 	io._setDebugOutput(debug);
 	if (!debug) return;
 });
+
+spam.command('pull')
+	.description('Pull changes from all managed repositories')
+	.action(async function spam_pull() {
+		await runGit({ concurrency: 4, args: ['pull', '--progress'] });
+	});
 
 export default spam;
